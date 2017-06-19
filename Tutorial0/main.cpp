c5aa11eeb374680800d43f3def4f0a1c8df5ccea
asm128 <pabloarielz@outlook.com>
2017-06-18 22:45:37 -0300

    + Improved comments. + Replaced old gpftw_ascii_color.h macros with proper enums. + Replaced old invert_color_ascii() macro with the more suitable ascii_color_invert() function.

--- Tutorial0/main.cpp
+++ Tutorial0/main.cpp
@@ -7,43 +7,43 @@
 #include <stdio.h>      // for printf()
 #include <windows.h>    // for interacting with Windows
 
-static constexpr	const int					SCREEN_WIDTH						= 48	;
-static constexpr	const int					SCREEN_HEIGHT						= 32	;
+static constexpr	const int							SCREEN_WIDTH						= 48	;
+static constexpr	const int							SCREEN_HEIGHT						= 32	;
 
 struct SApplication {
-	::gpftw::SASCIIScreen							ASCIIScreen							= {};
-	int												FrameCounter						= 0;	// Declare and initialize a variable of (int)eger type for keeping track of the number of frame since execution began.
+	::gpftw::SASCIIScreen									ASCIIScreen							= {};
+	int														FrameCounter						= 0;	// Declare and initialize a variable of (int)eger type for keeping track of the number of frame since execution began.
 };
 
 // Define some functions to use from main(). These functions will contain our game code.
-void											cleanup								(::SApplication& applicationInstance)				{ ::gpftw::destroyConsole(applicationInstance.ASCIIScreen);									}	// Cleanup application resources.
-void											setup								(::SApplication& applicationInstance)				{ ::gpftw::createConsole(applicationInstance.ASCIIScreen, ::SCREEN_WIDTH, ::SCREEN_HEIGHT);	}	// Initialize console.
-void											update								(::SApplication& applicationInstance)				{ ++applicationInstance.FrameCounter;														}	// Increase our frame counter by 1.
-void											draw								(::SApplication& applicationInstance)				{	
+void													cleanup								(::SApplication& applicationInstance)				{ ::gpftw::destroyConsole(applicationInstance.ASCIIScreen);									}	// Cleanup application resources.
+void													setup								(::SApplication& applicationInstance)				{ ::gpftw::createConsole(applicationInstance.ASCIIScreen, ::SCREEN_WIDTH, ::SCREEN_HEIGHT);	}	// Initialize console.
+void													update								(::SApplication& applicationInstance)				{ ++applicationInstance.FrameCounter;														}	// Increase our frame counter by 1.
+void													draw								(::SApplication& applicationInstance)				{	
 	// This function now will draw some coloured symbols in each cell of the ASCII screen.
-	::gpftw::SASCIIScreen								&asciiTarget						= applicationInstance.ASCIIScreen;
-	uint32_t											color0								= (0xFF & applicationInstance.FrameCounter * 1) | ((0xFF & applicationInstance.FrameCounter * 5) << 8) | ((0xFF & applicationInstance.FrameCounter * 2) << 16);
-	uint32_t											color1								= (0xFF & applicationInstance.FrameCounter * 2) | ((0xFF & applicationInstance.FrameCounter * 3) << 8) | ((0xFF & applicationInstance.FrameCounter * 1) << 16);
-	uint32_t											color2								= (0xFF & applicationInstance.FrameCounter * 3) | ((0xFF & applicationInstance.FrameCounter * 2) << 8) | ((0xFF & applicationInstance.FrameCounter * 5) << 16);
-	uint32_t											color3								= (0xFF & applicationInstance.FrameCounter * 5) | ((0xFF & applicationInstance.FrameCounter * 1) << 8) | ((0xFF & applicationInstance.FrameCounter * 3) << 16);
-	asciiTarget.Palette[COLOR_DARKGREY	]			= color0;
-	asciiTarget.Palette[COLOR_LIGHTGREY	]			= color1;
-	asciiTarget.Palette[COLOR_GREEN		]			= color2;
-	asciiTarget.Palette[COLOR_BLUE		]			= color3;
+	::gpftw::SASCIIScreen										&asciiTarget						= applicationInstance.ASCIIScreen;
+	uint32_t													color0								= (0xFF & applicationInstance.FrameCounter * 1) | ((0xFF & applicationInstance.FrameCounter * 17) << 8) | ((0xFF & applicationInstance.FrameCounter * 19) << 16);
+	uint32_t													color1								= (0xFF & applicationInstance.FrameCounter * 2) | ((0xFF & applicationInstance.FrameCounter * 13) << 8) | ((0xFF & applicationInstance.FrameCounter * 31) << 16);
+	uint32_t													color2								= (0xFF & applicationInstance.FrameCounter * 3) | ((0xFF & applicationInstance.FrameCounter * 11) << 8) | ((0xFF & applicationInstance.FrameCounter * 29) << 16);
+	uint32_t													color3								= (0xFF & applicationInstance.FrameCounter * 5) | ((0xFF & applicationInstance.FrameCounter *  7) << 8) | ((0xFF & applicationInstance.FrameCounter * 23) << 16);
+	asciiTarget.Palette[::gpftw::ASCII_COLOR_INDEX_0]		= color0;
+	asciiTarget.Palette[::gpftw::ASCII_COLOR_INDEX_1]		= color1;
+	asciiTarget.Palette[::gpftw::ASCII_COLOR_INDEX_2]		= color2;
+	asciiTarget.Palette[::gpftw::ASCII_COLOR_INDEX_3]		= color3;
 
 	// 2d loop
 	for(uint32_t y = 0; y < asciiTarget.Height	; ++y)
 	for(uint32_t x = 0; x < asciiTarget.Width	; ++x) {
-		uint32_t											linearIndex							= y * asciiTarget.Width	+ x;	// calculate the linear index of the screen array from the current x and y of the screen
+		uint32_t													linearIndex							= y * asciiTarget.Width	+ x;	// calculate the linear index of the screen array from the current x and y of the screen
 		
 		// Set the target cell with a given character and color.
-		asciiTarget.Characters	[linearIndex]			= (((applicationInstance.FrameCounter % 2) + y + x) % 2) ? '!' : '?';
-		asciiTarget.Colors		[linearIndex]			= (((applicationInstance.FrameCounter % 2) + y + x) % 2) ? COLOR_GREEN | (COLOR_DARKGREY << 4) : COLOR_BLUE | (COLOR_LIGHTGREY << 4) ;
+		asciiTarget.Characters	[linearIndex]					= (((applicationInstance.FrameCounter % 2) + y + x) % 2) ? '!' : '?';
+		asciiTarget.Colors		[linearIndex]					= (((applicationInstance.FrameCounter % 2) + y + x) % 2) ? ::gpftw::ASCII_COLOR_INDEX_0 | (::gpftw::ASCII_COLOR_INDEX_2 << 4) : ::gpftw::ASCII_COLOR_INDEX_1 | (::gpftw::ASCII_COLOR_INDEX_3 << 4) ;
 	}
 }
 
-int												main								()													{
-	::SApplication										* applicationInstance				= new ::SApplication();	// Create a new instance of our application.
+int														main								()													{
+	::SApplication												* applicationInstance				= new ::SApplication();	// Create a new instance of our application.
 	if( 0 == applicationInstance )
 		return -1;	// return error because we couldn't allocate the main instance of our application.
 
@@ -65,7 +65,7 @@ int												main								()													{
 	return 0; /// Exit from the function returning an (int)eger.
 }
 
-int	WINAPI										WinMain								
+int	WINAPI												WinMain								
 	(	_In_		HINSTANCE		// hInstance
 	,	_In_opt_	HINSTANCE		// hPrevInstance
 	,	_In_		LPSTR			// lpCmdLine
