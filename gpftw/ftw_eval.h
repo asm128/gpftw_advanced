3c18d155b1c7ec9fa7ce0cebba331bfb9d96a75a
asm128 <pabloarielz@outlook.com>
2017-06-23 08:15:50 -0300

    * Minor clarification.

--- gpftw/ftw_eval.h
+++ gpftw/ftw_eval.h
@@ -3,8 +3,8 @@
 
 namespace ftwlib 
 {
-	template<typename _tValue>	static inline constexpr	const _tValue&					min				(const _tValue& a, const _tValue& b)			noexcept	{ return a < b ? a : b; }
-	template<typename _tValue>	static inline constexpr	const _tValue&					max				(const _tValue& a, const _tValue& b)			noexcept	{ return a > b ? a : b; }
+	template<typename _tValue>	static inline constexpr	const _tValue&					min				(const _tValue& a, const _tValue& b)			noexcept	{ return (a < b) ? a : b; }
+	template<typename _tValue>	static inline constexpr	const _tValue&					max				(const _tValue& a, const _tValue& b)			noexcept	{ return (a > b) ? a : b; }
 }
 
 #endif // FTW_EVAL_H_97840928734902837409
